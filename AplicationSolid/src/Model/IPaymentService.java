1405c3d9047c7d70e0b532eadd1a73f149f65b5d
IrvingDev26 <irvingdeveloper26@hotmail.com>
2025-06-16 20:20:39 -0600

    Delete AplicationSolid directory

--- AplicationSolid/src/Model/IPaymentService.java
+++
@@ -1,7 +0,0 @@
-
-package Model;
-
-public interface IPaymentService {
-
-    boolean process(IPaymentMethod method, double amount);
-}
--- AplicationSolid/src/Model/PaymentMethod.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-
-public interface PaymentMethod {
-
-    void pay(double amount);
-}
